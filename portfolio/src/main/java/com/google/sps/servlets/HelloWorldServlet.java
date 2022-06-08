51d3a2ffdfab82c156c305f29dd178adc86afc27
Marco <marcolam1111@gmail.com>
2022-06-08 21:42:28 +0000

    Create ArrayList of Strings of movie quotes. Create function to convert ArrayList to Json with Gson.

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -5,14 +5,30 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
-@WebServlet("/hello")
+@WebServlet("/MovieQuotes")
 public class HelloWorldServlet extends HttpServlet {
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Marco!</h1>");
+    ArrayList<String> quotesArray = new ArrayList<String>();
+    quotesArray.add("“Carpe diem. Seize the day, boys. Make your lives extraordinary.” - Dead Poets Society");
+    quotesArray.add("“Just keep swimming.” -Finding Nemo");
+    quotesArray.add("“Jessica, only child, Illinois, Chicago.” - Parasite");
+
+    String json = convertToJsonUsingGson(quotesArray);
+
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
+
+  private String convertToJsonUsingGson(ArrayList<String> newString) {
+      Gson gson = new Gson();
+      String json = gson.toJson(newString);
+      return json;
+
+  }
+
 }
