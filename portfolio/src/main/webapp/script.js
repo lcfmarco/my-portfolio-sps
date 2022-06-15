28a9bd98a2262b807c302fa5cbb5f2b1ba553108
Marco <marcolam1111@gmail.com>
2022-06-15 22:12:06 +0000

    Add createMap() function

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -38,4 +38,10 @@ async function showQuote() {
     //console.log();
 }
 
+function createMap() {
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {center: {lat: 37.422, lng: -122.084}, zoom: 16});    
+}
+
 
